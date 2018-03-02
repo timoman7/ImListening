41214737cf4ab83a132ea4b36a4fef2822d1b063
Tim C <timoman8@hotmail.com>
2018-03-02 13:28:48 -0500

    Modifying ohgyun's makeEventable script

--- Listening.js
+++ Listening.js
@@ -132,4 +132,143 @@ function EObject(){};
       }
     }
   };
+  function getEventMap(){
+    this.__eventMap = this.__eventMap || {};
+    return this.__eventMap;
+  }
+  function validateEventExistence(name){
+    let eventMap = getEventMap.call(this);
+    if (typeof eventMap[name] === 'undefined') {
+      throw new Error('The event "' + name + '" is not defined.');
+    }
+  }
+  function shallowcopy(dest, source){
+    if(!source){
+      return dest;
+    }
+
+    for(let i in source){
+      if(source.hasOwnProperty(i)){
+        dest[i] = source[i];
+      }
+    }
+
+    return dest;
+  }
+  function register(name, handler, opts){
+    validateEventExistence.call(this, name);
+
+    if (typeof handler !== 'function') {
+      throw new Error('The handler of "' + name + '" should be a function.');
+    }
+
+    let eventMap = getEventMap.call(this);
+
+    let info = shallowcopy({
+      func: handler
+    }, opts);
+
+    eventMap[name].push(info);
+  }
+
+  Object.defineProperties(Object.prototype, {
+    'defineEvent': {
+      value: function(name){
+        let eventMap = getEventMap.call(this);
+        eventMap[name] = [];
+        return this;
+      },
+      writable: true,
+      enumerable: false
+    },
+    'on': {
+      value: function(name, handler, opts){
+        register.call(this, name, handler);
+        return this;
+      },
+      writable: true,
+      enumerable: false
+    },
+    'once': {
+      value: function(name, handler){
+        register.call(this, name, handler, {
+          once: true
+        });
+        return this;
+      },
+      writable: true,
+      enumerable: false
+    },
+    'trigger': {
+      value: function(name/*, args ... */){
+        validateEventExistence.call(this, name);
+
+        let eventMap = getEventMap.call(this);
+
+        let handlerInfos = eventMap[name].slice(), // use copied
+          args = Array.prototype.slice.call(arguments, 1),
+          i = 0,
+          len = handlerInfos.length,
+          info;
+
+        for (let i = 0; i < len; i++) {
+          info = handlerInfos[i];
+          info.func.apply(this, args);
+
+          if (info.once === true) {
+            // splice from original array
+            eventMap[name].splice(i, 1);
+          }
+        }
+
+        return this;
+      },
+      writable: true,
+      enumerable: false
+    }
+  });
 })();
+
+/*
+Object.prototype.defineEvent = function(name){
+  let eventMap = getEventMap.call(this);
+  eventMap[name] = [];
+  return this;
+};
+
+Object.prototype.on = function(name, handler, opts){
+  register.call(this, name, handler);
+  return this;
+};
+
+Object.prototype.once = function(name, handler){
+  register.call(this, name, handler, {
+    once: true
+  });
+  return this;
+};
+
+Object.prototype.trigger = function(name){
+  validateEventExistence.call(this, name);
+
+  let eventMap = getEventMap.call(this);
+
+  let handlerInfos = eventMap[name].slice(), // use copied
+    args = Array.prototype.slice.call(arguments, 1),
+    i = 0,
+    len = handlerInfos.length,
+    info;
+
+  for (let i = 0; i < len; i++) {
+    info = handlerInfos[i];
+    info.func.apply(this, args);
+
+    if (info.once === true) {
+      // splice from original array
+      eventMap[name].splice(i, 1);
+    }
+  }
+
+  return this;
+};
+*/
